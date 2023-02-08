af3fca46af73d3cc861cf049d1d97a2e53c6a6e0
Ann Shcherbakova <animal_spirit@mail.ru>
2023-02-08 11:19:24 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> <head>
+<html <?php language_attributes(); ?>>
+<head>
 <meta charset=" <?php bloginfo('charset') ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
